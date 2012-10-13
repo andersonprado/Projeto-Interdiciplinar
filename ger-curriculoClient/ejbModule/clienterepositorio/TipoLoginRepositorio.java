d60ae141e9b15132885fba03460a20622b0ba7cc
Anderson Luiz de Almeida Prado <andersonlprado@gmail.com>
2012-10-13 02:01:08 -0300

    Alteracoes provisorias

--- ger-curriculoClient/ejbModule/clienterepositorio/TipoLoginRepositorio.java
+++ ger-curriculoClient/ejbModule/clienterepositorio/TipoLoginRepositorio.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 import br.com.entidade.TipoLogin;
 
-
 public interface TipoLoginRepositorio {
 	public void adiciona(TipoLogin tipo);
+
+	public TipoLogin getTipo(int codigo);
 
 	public List<TipoLogin> getTipoLogin();
 }
